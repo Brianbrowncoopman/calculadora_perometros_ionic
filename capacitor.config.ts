6453a9eaac3297fb870986a2b0c70ae50e90386d
brianbrown <brianbrowncoopman@gmail.com>
2025-12-08 13:45:29 -0300

    carga inicial evaluacion 1

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.perimetrofiguras',
+  appName: 'perimetrofiguras',
+  webDir: 'www'
+};
+
+export default config;
