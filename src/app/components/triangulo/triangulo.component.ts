735b1a86643a1b7a6e1ab446822390fb46a25df1
brianbrown <brianbrowncoopman@gmail.com>
2025-12-08 14:11:34 -0300

    se crean las carpetas components con triangulo y circulo . se crea la carpeta modelo con las clases de typeScript

---
+++ src/app/components/triangulo/triangulo.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-triangulo',
+  templateUrl: './triangulo.component.html',
+  styleUrls: ['./triangulo.component.scss'],
+})
+export class TrianguloComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
